349c9239723cc06384491085c3c9de801a5ac9c6
amigin <amigin@gmail.com>
2022-02-09 15:50:32 +0200

    First Commit

---
+++ MyNoSqlDataReader.HttpClient/HttpContracts.cs
@@ -0,0 +1,107 @@
+using System.Text;
+using MyNoSqlDataReader.Core.SyncEvents;
+
+namespace MyNoSqlDataReader.HttpClient;
+
+public static class HttpContracts
+{
+    private static SyncContract? Parse(ref int pos, byte[] payload)
+    {
+        var header = ParsePascalString(ref pos, payload);
+        
+        if (header == null)
+        {
+            return null;
+        }
+
+        var (syncEventType, tableName) = ParseHeaderString(header);
+
+        var resultPayload = ParseByteArray(ref pos, payload);
+
+        return new SyncContract(tableName, syncEventType, resultPayload);
+    }
+    
+    public static IEnumerable<SyncContract> Read(byte[] payload)
+    {
+        var position = 0;
+
+        var syncContract = Parse(ref position, payload);
+        while (syncContract != null)
+        {
+            yield return syncContract;
+            syncContract = Parse(ref position, payload);
+        }
+    }
+    
+    
+    private static SyncEventType ParseSyncEventType(string src)
+    {
+
+        if (src == "initTable")
+            return SyncEventType.InitTable;
+        
+        if (src == "initPartitions")
+            return SyncEventType.InitPartitions;
+        
+        if (src == "updateRows")
+            return SyncEventType.UpdateRows;
+        
+        if (src == "deleteRows")
+            return SyncEventType.DeleteRows;
+
+        throw new Exception("Invalid Header Change Event Type");
+
+    }
+
+
+    private static string? ParsePascalString(ref int pos, byte[] payload)
+    {
+        if (pos >= payload.Length)
+            return null;
+        
+        var len = payload[pos];
+        pos++;
+
+        var result = Encoding.UTF8.GetString(payload.AsSpan(pos, len));
+        pos += len;
+
+        return result;
+    }
+
+    private static int ParseInt(ref int pos, byte[] payload)
+    {
+        var result = BitConverter.ToUInt16(payload.AsSpan(pos, pos+4));
+        pos += 4;
+
+        return result;
+    }
+
+    private static ReadOnlyMemory<byte> ParseByteArray(ref int pos, byte[] payload)
+    {
+        var len = ParseInt(ref pos, payload);
+
+        var result = new ReadOnlyMemory<byte>(payload, pos,  len);
+        pos += pos + len;
+
+        return result;
+    }
+
+
+    private static (SyncEventType eventType, string tableName) ParseHeaderString(string headerString)
+    {
+        var index = headerString.IndexOf('=');
+        
+        if (index<0)
+        {
+            throw new Exception($"Invalid header string {headerString}" );
+        }
+
+        var tableName = headerString.Substring(index + 1);
+
+        var eventName = headerString.Substring(0, index);
+
+        var eventType = ParseSyncEventType(eventName);
+
+        return (eventType, tableName);
+    } 
+}
---
+++ MyNoSqlDataReader.HttpClient/MyNoSqlHttpConnection.cs
@@ -0,0 +1,216 @@
+﻿using Flurl;
+using Flurl.Http;
+using MyNoSqlDataReader.Core;
+using MyNoSqlDataReader.Core.SyncEvents;
+
+namespace MyNoSqlDataReader.HttpClient;
+
+using MyNoSqlServer.Abstractions;
+
+public class MyNoSqlHttpConnection
+{
+    public enum LogLevel{
+        Error, Info
+    }
+
+    public struct LogItem
+    {
+        public DateTime DateTime { get; private set; }
+        public LogLevel Level { get; private set; }
+        public string Process { get; private set; }
+        public string Message { get; private set; }
+        public Exception? Exception { get; private set; }
+        
+        public static LogItem Create(LogLevel logLevel, string process, string message, Exception? exception)
+        {
+            return new LogItem
+            {
+                DateTime = DateTime.UtcNow,
+                Level = logLevel,
+                Process = process,
+                Message = message,
+                Exception = exception
+            };
+        }
+    }
+    
+    public class NewSessionModel
+    {
+        public string? Session { get; set; }
+    }
+
+    private readonly Func<string> _getHost;
+    private string _appName;
+    private string _libVersion;
+
+    private readonly Dictionary<string, IMyNoSqlDataReader> _subscribers = new();
+    
+    public TimeSpan ConnectTimeout = TimeSpan.FromSeconds(3);
+    
+    public MyNoSqlHttpConnection(Func<string> getHost, string appName, string libVersion)
+    {
+        _getHost = getHost;
+        _appName = appName;
+        _libVersion = libVersion;
+    }
+
+
+
+    private readonly object _lockObject = new();
+    private Task? _connectLoopTask;
+
+    private string? _sessionId;
+
+    private Action<LogItem>? _logCallback;
+
+    private void PlugLog(Action<LogItem> logCallback)
+    {
+        _logCallback = logCallback;
+    }
+
+
+    private void WriteLogToConsole(ref LogItem item)
+    {
+        Console.WriteLine("==== Socket Log Record =====");
+        Console.WriteLine($"DateTime: {item.DateTime:s}");
+        Console.WriteLine($"Level: {item.Level}");
+        Console.WriteLine($"Process: {item.Process}");
+        Console.WriteLine($"Message: {item.Message}");
+
+        if (item.Exception != null)
+        {
+            Console.WriteLine($"Exception: {item.Exception}");
+        }
+    }
+
+
+    private void WriteLog(LogItem item)
+    {
+        if (_logCallback == null)
+        {
+            WriteLogToConsole(ref item);
+        }
+        else
+        {
+            if (item.Level == LogLevel.Error)
+            {
+                WriteLogToConsole(ref item);
+            }
+
+            _logCallback(item);
+        }
+    }
+
+    private async Task ConnectLoopAsync()
+    {
+
+        while (true)
+        {
+            try
+            {
+                if (_sessionId == null)
+                    await GetSessionAsync();
+
+                await SubscribeToTablesAsync();
+
+                await GetChangesLoopAsync();
+
+            }
+            catch (Exception e)
+            {
+                WriteLog(LogItem.Create(LogLevel.Error, "ConnectLoop", e.Message, e));
+                await Task.Delay(ConnectTimeout);
+            }
+            
+        }
+        
+    }
+
+    public MyNoSqlDataReader<TDbRow> Subscribe<TDbRow>(string tableName) where TDbRow: IMyNoSqlEntity, new()
+    {
+        var parser = new HttpEventsParser<TDbRow>();
+        var result = new MyNoSqlDataReader<TDbRow>(tableName, parser);
+        _subscribers.Add(tableName, result);
+        return result;
+    }
+
+
+    private async Task GetChangesLoopAsync()
+    {
+        try
+        {
+            while (true)
+            {
+                await GetChangesAsync();
+            }
+        }
+        finally
+        {
+            WriteLog(LogItem.Create(LogLevel.Info, "Session is disconnected", $"Id={_sessionId}", null));
+            _sessionId = null;
+        }
+
+    }
+
+    private async Task SubscribeToTablesAsync()
+    {
+        foreach (var tableName in _subscribers.Keys)
+        {
+            await _getHost().AppendPathSegment("DataReader")
+                .AppendPathSegment("Subscribe")    
+                .WithHeader("session", _sessionId)
+                .SetQueryParam("tableName", tableName)
+                .PostAsync();
+        }
+    }
+
+    private async Task GetChangesAsync()
+    {
+        var syncPayload = await _getHost().AppendPathSegment("DataReader")
+            .AppendPathSegment("GetChanges")
+            .WithHeader("session", _sessionId)
+            .PostAsync().ReceiveBytes();
+
+
+        foreach (var syncContract in HttpContracts.Read(syncPayload))
+        {
+            if (_subscribers.TryGetValue(syncContract.TableName, out var dataReader))
+            {
+                dataReader.UpdateData(syncContract);
+            }
+        }
+
+    }
+
+    private async Task GetSessionAsync()
+    {
+        var result = await _getHost().AppendPathSegment("DataReader")
+            .AppendPathSegment("Greeting")
+            .SetQueryParam("name", "Test")
+            .SetQueryParam("version", "2.3.4")
+            .PostAsync().ReceiveJson<NewSessionModel>();
+
+        _sessionId =  result.Session;
+        
+        WriteLog(LogItem.Create(LogLevel.Info, "New Session", $"Id={_sessionId}", null));
+    }
+
+    public void Start()
+    {
+
+        lock (_lockObject)
+        {
+
+            if (_subscribers.Count == 0)
+            {
+                throw new Exception("There are no subscribers to start MyNoSqlDataReader");
+            }
+            
+            if (_connectLoopTask != null)
+                throw new Exception("You can not start read loop two times");
+
+            _connectLoopTask = ConnectLoopAsync();
+        }
+    }
+    
+}
